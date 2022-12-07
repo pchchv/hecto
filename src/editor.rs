651487f47677d865b5eb14d891b49dbb464c0b43
pchchv <ipchchv@gmail.com>
2022-12-07 16:43:50 +0300

    Editor type

---
+++ src/editor.rs
@@ -0,0 +1,13 @@
+use crate::Terminal;
+use termion::event::Key;
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
+pub struct Editor {
+    should_quit: bool,
+    terminal: Terminal,
+    cursor_position: Position,
+}
