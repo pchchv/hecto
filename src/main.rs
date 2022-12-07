37b3622e49bc8595ce0c9d1c4753ed891fc5e0ae
pchchv <ipchchv@gmail.com>
2022-12-07 17:03:40 +0300

    Main func. Run editor

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
-use std::io::{self, stdout};
-use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+mod terminal;
+use editor::Editor;
+pub use editor::Position;
+pub use terminal::Terminal;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?}\r", c as u8);
-                    } else {
-                        println!("{:?} ({})\r", c as u8, c);
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => panic!("{}", err),
-        }
-    }
+    Editor::default().run();
 }
