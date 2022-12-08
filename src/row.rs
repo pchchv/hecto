55de3f59e3bcc76a2ad104e40d382d352785fcc0
pchchv <ipchchv@gmail.com>
2022-12-08 16:11:36 +0300

    Update Row Insert func

--- src/row.rs
+++ src/row.rs
@@ -22,6 +22,7 @@ impl Row {
         let start = cmp::min(start, end);
         let mut result = String::new();
 
+        #[allow(clippy::integer_arithmetic)]
         for grapheme in self.string[..]
             .graphemes(true)
             .skip(start)
@@ -45,23 +46,28 @@ impl Row {
         self.len == 0
     }
 
-    fn update_len(&mut self) {
-        self.len = self.string[..].graphemes(true).count();
-    }
-
     pub fn insert(&mut self, at: usize, c: char) {
         if at >= self.len() {
             self.string.push(c);
-        } else {
-            let mut result: String = self.string[..].graphemes(true).take(at).collect();
-            let remainder: String = self.string[..].graphemes(true).skip(at).collect();
-
-            result.push(c);
-            result.push_str(&remainder);
-
-            self.string = result;
+            self.len += 1;
+            return;
         }
 
-        self.update_len();
+        let mut result: String = String::new();
+        let mut length = 0;
+
+        for (index, grapheme) in self.string[..].graphemes(true).enumerate() {
+            length += 1;
+            if index == at {
+                length += 1;
+                result.push(c);
+            }
+
+            result.push_str(grapheme);
+        }
+
+        self.len = length;
+
+        self.string = result;
     }
 }
