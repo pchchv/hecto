be4c2658f894b07f98fe3a4a2768aa66c315dca1
pchchv <ipchchv@gmail.com>
2022-12-08 15:47:31 +0300

    Insert func of Row type

--- src/row.rs
+++ src/row.rs
@@ -50,4 +50,20 @@ impl Row {
     fn update_len(&mut self) {
         self.len = self.string[..].graphemes(true).count();
     }
+
+    pub fn insert(&mut self, at: usize, c: char) {
+        if at >= self.len() {
+            self.string.push(c);
+        } else {
+            let mut result: String = self.string[..].graphemes(true).take(at).collect();
+            let remainder: String = self.string[..].graphemes(true).skip(at).collect();
+
+            result.push(c);
+            result.push_str(&remainder);
+
+            self.string = result;
+        }
+
+        self.update_len();
+    }
 }
